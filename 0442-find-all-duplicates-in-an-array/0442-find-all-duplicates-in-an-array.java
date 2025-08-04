6316d2f1ddcc9d2e11507c66117a3925e8eb1d09
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-08-04 17:15:41 +0500

    Update 0442-find-all-duplicates-in-an-array.java

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,12 +1,13 @@
+// sign flipping (negative marking)
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> result = new ArrayList<>();
         for (int v : nums) {
-            int idx = Math.abs(v) - 1;
-            if (nums[idx] < 0) {
+            int index = Math.abs(v) - 1;
+            if (nums[index] < 0) {
                 result.add(Math.abs(v));
             } else {
-                nums[idx] = -nums[idx];
+                nums[index] = -nums[index];
             }
         }
         return result;
