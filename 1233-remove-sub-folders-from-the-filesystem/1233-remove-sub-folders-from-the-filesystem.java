a10fadd712743c47c4da95d89fb5715a46f2ddcc
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-08-04 17:25:47 +0500

    Update 1233-remove-sub-folders-from-the-filesystem.java

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,3 +1,4 @@
+// Sort folder paths lexicographically, and skip folders that are subpaths of the last added folder.
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
         Arrays.sort(folder);
@@ -10,3 +11,6 @@ class Solution {
         return result;
     }
 }
+
+// Time: O(n log n)
+// Space: O(n)
