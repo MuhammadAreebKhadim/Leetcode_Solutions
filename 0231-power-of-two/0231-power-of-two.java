f54d5758f9b0d7da5c8307d7f2117749fca7e470
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-08-06 13:00:20 +0500

    Update 0231-power-of-two.java

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,6 +1,12 @@
+// Bit Manupliation: n = 8(1000); n-1 = 7(0111); 1000 & 0111 = 0000 
 class Solution {
     public boolean isPowerOfTwo(int n) {
         return n > 0 && (n & ( n-1)) == 0;
+// Time = O(1)
+// Space = O(1)
+
+
+        
         // if (n <= 0) return false;
         // while (n != 1 ) {
         //     if(n % 2 != 0){
