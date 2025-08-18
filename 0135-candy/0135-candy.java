3d5d8e130a439a32ba08948e1fededa8b65500fe
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-08-18 13:36:41 +0500

    Time: 2 ms (99.41%), Space: 45.6 MB (81.52%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -2,24 +2,20 @@ class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
         int[] candies = new int[n];
-
         // everyone gets at least 1
         for (int i = 0; i < n; i++) candies[i] = 1;
-
         // left -> right: handle ascending chains
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
                 candies[i] = candies[i - 1] + 1;
             }
         }
-
         // right -> left: handle descending chains
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
                 candies[i] = Math.max(candies[i], candies[i + 1] + 1);
             }
         }
-
         int sum = 0;
         for (int c : candies) sum += c;
         return sum;
