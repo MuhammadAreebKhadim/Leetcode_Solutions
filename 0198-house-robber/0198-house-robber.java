aa4faa48272234f5419ab2eaacfad01feb5f49ca
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-08-25 19:41:36 +0500

    Time: 0 ms (100%), Space: 41.3 MB (34.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,27 @@
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         if(n == 1) return nums[0];
+//         int[] rob = new int[n];
+//         rob[0] = nums[0];
+//         rob[1] = Math.max(nums[0], nums[1]);
+//         for(int i = 2; i < n; i++){
+//             rob[i] = Math.max(rob[i - 1], rob[i - 2] + nums[i]);
+//         }
+//         return rob[n - 1];
+//     }
+// }
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
-        int[] rob = new int[n];
-        rob[0] = nums[0];
-        rob[1] = Math.max(nums[0], nums[1]);
+        int pre2 = nums[0];
+        int pre1 = Math.max(nums[0], nums[1]);
         for(int i = 2; i < n; i++){
-            rob[i] = Math.max(rob[i - 1], rob[i - 2] + nums[i]);
+            int current = Math.max(pre1, pre2 + nums[i]);
+            pre2 = pre1;
+            pre1 = current;
         }
-        return rob[n - 1];
+        return pre1;
     }
 }
