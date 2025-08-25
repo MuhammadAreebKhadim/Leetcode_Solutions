2a6116ef9d62702905438c877da1503c346a893f
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-08-25 19:42:13 +0500

    Time: 0 ms (100%), Space: 41 MB (60.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,27 +1,32 @@
-// class Solution {
-//     public int rob(int[] nums) {
-//         int n = nums.length;
-//         if(n == 1) return nums[0];
-//         int[] rob = new int[n];
-//         rob[0] = nums[0];
-//         rob[1] = Math.max(nums[0], nums[1]);
-//         for(int i = 2; i < n; i++){
-//             rob[i] = Math.max(rob[i - 1], rob[i - 2] + nums[i]);
-//         }
-//         return rob[n - 1];
-//     }
-// }
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
-        int pre2 = nums[0];
-        int pre1 = Math.max(nums[0], nums[1]);
+        int[] rob = new int[n];
+        rob[0] = nums[0];
+        rob[1] = Math.max(nums[0], nums[1]);
         for(int i = 2; i < n; i++){
-            int current = Math.max(pre1, pre2 + nums[i]);
-            pre2 = pre1;
-            pre1 = current;
+            rob[i] = Math.max(rob[i - 1], rob[i - 2] + nums[i]);
         }
-        return pre1;
+        return rob[n - 1];
     }
 }
+
+
+
+// Second Approach:
+
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         if(n == 1) return nums[0];
+//         int pre2 = nums[0];
+//         int pre1 = Math.max(nums[0], nums[1]);
+//         for(int i = 2; i < n; i++){
+//             int current = Math.max(pre1, pre2 + nums[i]);
+//             pre2 = pre1;
+//             pre1 = current;
+//         }
+//         return pre1;
+//     }
+// }
