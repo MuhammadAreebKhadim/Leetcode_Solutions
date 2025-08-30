1008b478fde8df987a1f5ef1a750f4c215f66ced
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-08-30 17:04:22 +0500

    Time: 21 ms (92.92%), Space: 55.8 MB (9.72%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,13 +1,19 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int[][] res = new int[k][2];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> (x[0]*x[0] + x[1]*x[1]) - (y[0]*y[0] + y[1]*y[1]));
-        for(int[] p : points){
-            pq.offer(p);
-        }
-        for(int i = 0; i < k; i++){
-            res[i] = pq.poll();
-        }
-        return res;
+        // Sort Approach:
+        Arrays.sort(points, (x, y) -> (x[0]*x[0] + x[1]*x[1]) - (y[0]*y[0] + y[1]*y[1]));
+        return Arrays.copyOfRange(points, 0, k);
+
+
+        // Heap Approach:
+        // int[][] res = new int[k][2];
+        // PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> (x[0]*x[0] + x[1]*x[1]) - (y[0]*y[0] + y[1]*y[1]));
+        // for(int[] p : points){
+        //     pq.offer(p);
+        // }
+        // for(int i = 0; i < k; i++){
+        //     res[i] = pq.poll();
+        // }
+        // return res;
     }
 }
