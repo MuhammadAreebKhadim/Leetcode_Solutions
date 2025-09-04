b3fa7f1ae300e602483219abfc57627cd7dc5236
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-09-04 17:04:10 +0500

    Time: 1 ms (92.67%), Space: 44.5 MB (82.52%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -4,17 +4,17 @@ class Solution {
         backtrack(nums, new ArrayList<Integer>(), new boolean[nums.length], result);
         return result;
     }
-    private void backtrack(int[] nums, List<Integer>path, boolean[] used, List<List<Integer>> result){
-        if(path.size() == nums.length){
-            result.add(new ArrayList<>(path));
+    private void backtrack(int[] nums, List<Integer>perm, boolean[] used, List<List<Integer>> result){
+        if(perm.size() == nums.length){
+            result.add(new ArrayList<>(perm));
             return;
         }
         for(int i = 0; i < nums.length; i++){
             if(used[i]) continue;
             used[i] = true;
-            path.add(nums[i]);
-            backtrack(nums, path, used, result);
-            path.remove(path.size() - 1);
+            perm.add(nums[i]);
+            backtrack(nums, perm, used, result);
+            perm.remove(perm.size() - 1);
             used[i] = false;
         }
     }
