8dc044b63b3d2ce24f067d3fb19394422f1c0a76
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-09-05 20:07:39 +0500

    Time: 370 ms (31.7%), Space: 40.9 MB (88.43%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        backtrack(board);
+    }
+    private boolean backtrack(char[][] board){
+        for(int row = 0; row < 9; row++){
+            for(int col = 0; col < 9; col++){
+                if(board[row][col] == '.'){
+                    for(char c = '1'; c <= '9'; c++){
+                        if(isvalid(board, row, col, c)){
+                            board[row][col] = c;
+                            if(backtrack(board)) return true;
+                            board[row][col] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private boolean isvalid(char[][] board, int row, int col, char c){
+        for(int i = 0; i < 9; i++){
+            if(board[row][i] == c) return false;
+            if(board[i][col] == c) return false;
+            if(board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+}
