8a3bde04ac2e852d2a763b01b068b9d0f6c48238
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-09-08 16:39:14 +0500

    Update 2390-removing-stars-from-a-string.java

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,26 +1,30 @@
 class Solution {
     public String removeStars(String s) {
-        // Stack<Character> st  = new Stack<>();
-        // for(char c : s.toCharArray()){
-        //     if(c == '*'){
-        //         st.pop();
-        //     } else{
-        //         st.push(c);
-        //     }
-        // }
-        // StringBuilder res = new StringBuilder();
-        // for(char ch : st){
-        //     res.append(ch);
-        // }
-        // return res.toString();
-        StringBuilder sb = new StringBuilder();
+        // Using Stack:
+        Stack<Character> st  = new Stack<>();
         for(char c : s.toCharArray()){
             if(c == '*'){
-                sb.deleteCharAt(sb.length() - 1);
+                st.pop();
             } else{
-                sb.append(c);
+                st.push(c);
             }
         }
-        return sb.toString();
+        StringBuilder res = new StringBuilder();
+        for(char ch : st){
+            res.append(ch);
+        }
+        return res.toString();
+
+        
+        // Using String:
+        // StringBuilder sb = new StringBuilder();
+        // for(char c : s.toCharArray()){
+        //     if(c == '*'){
+        //         sb.deleteCharAt(sb.length() - 1);
+        //     } else{
+        //         sb.append(c);
+        //     }
+        // }
+        // return sb.toString();
     }
 }
