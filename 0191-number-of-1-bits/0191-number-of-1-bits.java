efc247d84e15bf64d17ffc5145f4ecad6646c184
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-09-30 11:50:40 +0500

    Time: 0 ms (100%), Space: 41.3 MB (8.09%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,10 +1,12 @@
 class Solution {
     public int hammingWeight(int n) {
+// Brian Kernighan’s Algorithm:
         int count = 0;
-        while(n != 0){
-            count += n & 1;
-            n >>>= 1;
+        while(n != 0){  // Loop runs only as many times as there are 1s.
+            n &= n - 1;   // removes the lowest set bit from n
+            count++;
         }
         return count;
+        
     }
 }
