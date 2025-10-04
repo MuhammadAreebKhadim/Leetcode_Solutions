720e05ea33598809cae6487afbd3a4bc10025841
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-10-04 22:02:16 +0500

    Time: 0 ms (100%), Space: 40.3 MB (68.82%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num < 2) return true;
+        long left = 1, right = num / 2;
+        while(left <= right){
+            long mid = left + (right - left) / 2;
+            long sqr = mid * mid;
+            if(sqr == num) return true;
+            else if(sqr < num) left = mid + 1; 
+            else{
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
