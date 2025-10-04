89db7e66098dad604a47f4439c8191894f3193b4
Muhammad Areeb <muhammadareeb1087@gmail.com>
2025-10-04 22:08:41 +0500

    Time: 0 ms (100%), Space: 40.2 MB (84.17%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,16 +1,25 @@
+// Binary Search:
 class Solution {
     public boolean isPerfectSquare(int num) {
+        // long left = 1, right = num / 2;
+        // while(left <= right){
+        //     long mid = left + (right - left) / 2;
+        //     long sqr = mid * mid;
+        //     if(sqr == num) return true;
+        //     else if(sqr < num) left = mid + 1; 
+        //     else{
+        //         right = mid - 1;
+        //     }
+        // }
+        // return false;
+
+
+    // Newton-Raphson’s Method (Mathematical Optimization):
         if(num < 2) return true;
-        long left = 1, right = num / 2;
-        while(left <= right){
-            long mid = left + (right - left) / 2;
-            long sqr = mid * mid;
-            if(sqr == num) return true;
-            else if(sqr < num) left = mid + 1; 
-            else{
-                right = mid - 1;
-            }
+        long x = num;
+        while(x * x > num){
+            x = (x + num / x) / 2;
         }
-        return false;
+        return x * x == num;
     }
 }
